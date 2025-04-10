b2919ca817a2f3c38ff4599793e95aa6ab27e1f0
hajba-balint <98750569+hajba-balint@users.noreply.github.com>
2025-04-10 21:06:20 +0200

    Update Gamblers.cs

--- Gamblers.cs
+++ Gamblers.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,20 +11,36 @@ namespace GamblersDatabase
         public string Name { get; set; }
         public float CreditScore { get; set; }
         public string CardInfo { get; set; }
+        public string CardNum { get; set; }
+        public string CardExp { get; set; }
+        public string CardCVC { get; set; }
         public string Address { get; set; }
+        //public string City { get; set; }
+        //public string Street { get; set; }
+        //public string House { get; set; }
+        // nem mindenhol lehet, hibas txt
         public float Debt { get; set; }
         public string Birthday { get; set; }
+        public string BirthYear { get; set; }
+        public string BirthMonth { get; set; }
+        public string BirthDay { get; set; }
         public bool isMinor { get; set; }
 
-        public Gamblers(string name, float creditscore, string cardinfo, string address, float debt, string birthday, bool isminor)
+        public Gamblers(string Name, float CreditScore, string CardInfo, string Address, float Debt, string Birthday, bool isMinor)
         {
-            name = Name;
-            creditscore = CreditScore;
-            cardinfo = CardInfo;
-            address = Address;
-            debt = Debt;
-            birthday = Birthday;
-            isminor = isMinor;
+            this.Name = Name;
+            this.CreditScore = CreditScore;
+            this.CardInfo = CardInfo;
+            this.CardNum = this.CardInfo.Split(",")[0];
+            this.CardExp = this.CardInfo.Split(",")[1];
+            this.CardCVC = this.CardInfo.Split(",")[2];
+            this.Address = Address;
+            this.Birthday = Birthday;
+            this.BirthYear = this.Birthday.Split(".")[0];
+            this.BirthMonth = this.Birthday.Split(".")[1];
+            this.BirthDay = this.Birthday.Split(".")[2];
+            this.Address = Address;
+            this.isMinor = isMinor;
         }
     }
 }
